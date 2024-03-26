5f952e451737ea6bd2449f7f783890324403411e
최지현 <gus1043@gmail.com>
2024-03-26 15:49:40 +0900

    unknown, never타입

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    if (resultConversion === "as-number") {
-        return +result; //또는 paresFloat(result) 하면 숫자로 변환됨
-    }
-    else {
-        return result.toString();
-    }
+var userInput;
+var userName;
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-var combineAge = combine(20, 21, "as-number");
-console.log(combineAge);
-var combineStringAge = combine("20", "21", "as-number");
-console.log(combineStringAge);
-var combineNames = combine("mimi", "zizi", "as-text");
-console.log(combineNames);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+var result = generateError("An error occured!", 500);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,26 +1,13 @@
-function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: string
-) {
-  let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  if (resultConversion === "as-number") {
-    return +result; //또는 paresFloat(result) 하면 숫자로 변환됨
-  } else {
-    return result.toString();
-  }
+let userInput: unknown;
+let userName: string;
+
+if (typeof userInput === "string") {
+  userName = userInput;
 }
 
-const combineAge = combine(20, 21, "as-number");
-console.log(combineAge);
+function generateError(message: string, code: number) {
+  throw { message: message, errorCode: code };
+}
 
-const combineStringAge = combine("20", "21", "as-number");
-console.log(combineStringAge);
-
-const combineNames = combine("mimi", "zizi", "as-text");
-console.log(combineNames);
+const result = generateError("An error occured!", 500);
+console.log(result);
